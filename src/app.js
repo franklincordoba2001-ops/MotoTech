f121edfca4d8ffc553b5b997f537881194fcd5de
franklin cordoba <franklincordoba2001@gmail.com>
2026-03-08 16:21:05 -0500

    Actualización proyecto MotoTech

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 require('./config/db');
+
 // registro de rutas
 const clienteRoutes = require('./routes/clienteRoutes');
 const motoRoutes = require('./routes/motoRoutes');
--- src/config/db.js
+++ src/config/db.js
@@ -4,13 +4,13 @@ const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'tallermecanico'
+    database: 'mototech'
 });
 // Bloque de prueba de conexión
 (async () => {
     try {
         const conn = await connection.getConnection();
-        console.log(' Conectado  base de datos "tallermecanico" exitosamente.');
+        console.log(' Conectado  base de datos "mototech" exitosamente.');
         conn.release(); // Importante: libera la conexión de vuelta al pool
     } catch (error) {
         console.error(' Error al conectar a la base de datos:', error.message);
