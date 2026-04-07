87f5416284a901e6865218010a7a256f656e65a7
franklin cordoba <franklincordoba2001@gmail.com>
2026-04-07 17:44:00 -0500

    Se protegen rutas de usuarios con roles admin

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-
 const {
   createUsuario,
   getUsuarios,
-  deleteUsuario, 
+  deleteUsuario,
 } = require("../controllers/usuarioController");
 
-router.post("/", createUsuario);
-router.get("/", getUsuarios);
-router.delete("/:id", deleteUsuario);
+const verifyToken = require("../middleware/authMiddleware");
+const authorizeRole = require("../middleware/roleMiddleware");
+
+// SOLO ADMIN
+router.post("/", verifyToken, authorizeRole("admin"), createUsuario);
+
+router.get("/", verifyToken, authorizeRole("admin"), getUsuarios);
+
+router.delete("/:id", verifyToken, authorizeRole("admin"), deleteUsuario);
 
 module.exports = router;
